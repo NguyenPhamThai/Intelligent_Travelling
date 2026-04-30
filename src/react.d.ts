6ee7c81abae56266b58acfc40139baeedc117ccf
UNKN0W-Git <pro9snek@gmail.com>
2026-04-30 19:50:04 +0700

    Update react.d.ts

--- src/react.d.ts
+++ src/react.d.ts
@@ -1,1 +1,10 @@
-declare module "react";
+declare module "react" {
+  export type ReactNode = any;
+
+  export function useState(initial: any): any;
+
+  export function useMemo(factory: any, deps: any[]): any;
+
+  const React: any;
+  export default React;
+}
